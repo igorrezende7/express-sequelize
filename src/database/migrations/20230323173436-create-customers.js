931e4a810b8ae36428246fd18edacb984326c220
Igor Brasileiro <igor@salut.com.br>
2023-03-23 15:03:58 -0300

    first commit

---
+++ src/database/migrations/20230323173436-create-customers.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.createTable('costumers', {
+       id: {
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        autoIncrement:true
+       },
+       name:{
+        type:Sequelize.STRING,
+        allowNull:false
+       },
+       email:{
+        type:Sequelize.STRING,
+        unique:true,
+        allowNull:false
+       },
+       created_at:{
+        type:Sequelize.DATE,
+        allowNull:false
+       },
+       updated_at:{
+        type:Sequelize.DATE,
+        allowNull:false
+       }
+      }
+
+       );
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.dropTable('costumers');
+  }
+};
